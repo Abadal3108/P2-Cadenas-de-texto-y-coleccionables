ba7866c46210b6ca20a9f6787be84955c388584e
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-09-24 12:17:36 +0200

    EX_10 hecho

---
+++ src/EX_10/App.java
@@ -0,0 +1,19 @@
+package EX_10;
+
+import java.util.HashSet;
+
+public class App {
+
+    public static void main(String args[]) {
+
+        HashSet<String> hset = new HashSet<String>();
+        hset.add("alex");
+        hset.add("marc");
+        hset.add("nil");
+        hset.add("ramon");
+        hset.add("pepe");
+
+        System.out.println(hset);
+
+    }
+}
