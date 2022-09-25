96d59bc9b244cd00109e9e46f2706bef0c223515
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-09-25 12:58:56 +0200

    EX_4,5 problemas de logica, EX_NONAME DONE + QUESTION

---
+++ src/EX_NONAME/App.java
@@ -0,0 +1,22 @@
+package EX_NONAME;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List al = new ArrayList();
+        al.add("Elemento 1");
+        al.add("Elemento 2");
+        al.add("Elemento 3");
+
+        String[] ary = new String[al.size()];
+
+        //java pide cast? preguntar
+        ary = (String[]) al.toArray(ary);
+
+        for (String s : ary)
+            System.out.println(s);
+
+    }
+}
