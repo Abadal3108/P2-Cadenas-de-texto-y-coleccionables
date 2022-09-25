c1c188ef3509234be669ee637b528e708e117ccd
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-09-25 13:21:58 +0200

    BASE EX_10

--- src/EX_10/App.java
+++ src/EX_10/App.java
@@ -1,19 +1,40 @@
 package EX_10;
-
-import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Iterator;
+import java.util.ArrayList;
 
 public class App {
+    public static void main(String[] args) {
+        HashMap<String, String> numeros = new HashMap<String, String>();
 
-    public static void main(String args[]) {
+        //añadir valores al hashmap
+        numeros.put("uno", "1");
+        numeros.put("dos", "2");
+        numeros.put("tres", "3");
+        numeros.put("cuatro", "4");
+        //sout con hashmap
+        System.out.println(numeros);
 
-        HashSet<String> hset = new HashSet<String>();
-        hset.add("alex");
-        hset.add("marc");
-        hset.add("nil");
-        hset.add("ramon");
-        hset.add("pepe");
+        //creamos treemap de strings asignar el hashmap
+        TreeMap<String, String> tm = new TreeMap<String, String>(numeros);
+        Iterator itr = tm.keySet().iterator();
+        while (itr.hasNext()) {
 
-        System.out.println(hset);
+            //preguntar key si es el string0? o que es?
+            String key = (String) itr.next();
+            System.out.println("key: " + key + " ,value: " + numeros.get(key));
+        }
+        ArrayList<HashMap<String, String>> arrayListHashMap = new ArrayList<HashMap<String, String>>();
+        arrayListHashMap.add(numeros);
+        Iterator<HashMap<String, String>> it = arrayListHashMap.iterator();
 
+        while (it.hasNext()) {
+            System.out.println(it.next());
+
+
+        }
     }
-}
+    }
+
