87d599de5232d07a28f061ccb49654d3cd18460f
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-09-27 09:59:42 +0200

    BASE EX_05 perf

--- src/EX_05/App.java
+++ src/EX_05/App.java
@@ -15,33 +15,40 @@ public class App {
             Integer numeros = 0;
             System.out.println("Introduce contraseña, Consideramos una contraseña fuerte si contiene 8 o más caracteres, y entre\n" +
                     "ellos, por lo menos una mayúscula, una minúscula, un símbolo y un dígito.");
-            String contraseña = sc.toString();
+
+            String contraseña = sc.nextLine();
             String[] partes = contraseña.split("");
-            if (contraseña.length() <= 8) {
+            if (contraseña.length() >= 8) {
                 System.out.println("Tamaño correcto, contiene 8 o más caracteres");
+
+                for (int i = 0; i < contraseña.length(); i++) {
+                    if (Character.isUpperCase(contraseña.charAt(i))) { //Convierte una cadena de texto en un número entero.
+                        mayus++;
+                        System.out.println("+ " + mayus+" mayus");
+                    } else if (Character.isLowerCase(contraseña.charAt(i))) {
+                        minus++;
+                        System.out.println("+ " + minus+" minus");
+                    } else if (Character.isDigit(contraseña.charAt(i))) {
+                        numeros++;
+                        System.out.println("+ " + numeros+" num");
+
+                    } else {
+                        simbolos++;
+                        System.out.println("+ " + simbolos+" sim");
+                    }
+                }
             } else {
                 System.out.println("Tamaño incorrecto, necesita 8 o más caracteres");
 
             }
-            for (int i = 0; i < partes.length; i++) {
-                if (Character.isUpperCase(Integer.parseInt(partes[i]))) { //Convierte una cadena de texto en un número entero.
-                    mayus++;
-                } else if (Character.isLowerCase(Integer.parseInt(partes[i]))) {
-                    minus++;
-                } else if (Character.isDigit(Integer.parseInt(partes[i]))) {
-                    numeros++;
 
-                } else {
-                    simbolos++;
-                }
-            }
 
-            if (mayus >= 1 && minus >= 1 && numeros >= 1 && simbolos >= 1 && contraseña.length() <= 8) {
+            if (mayus >= 1 && minus >= 1 && numeros >= 1 && simbolos >= 1 && contraseña.length() >= 8) {
                 System.out.println("Contraseña fuerte");
                 correcto = true;
             }
         } while (!correcto);
-
+        System.out.println("correcto");
     }
 
 }
