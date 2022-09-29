08655ffc0cdd4cbca14f183a4c7268839ab93c7a
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-09-29 13:17:49 +0200

    all done and working good!!!

--- src/EX_10/App.java
+++ src/EX_10/App.java
@@ -1,40 +1,43 @@
 package EX_10;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.TreeMap;
-import java.util.Iterator;
+
+import java.security.KeyStore;
+import java.util.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class App {
     public static void main(String[] args) {
-        HashMap<String, String> numeros = new HashMap<String, String>();
+        HashMap<String, Integer> numeros = new HashMap<String, Integer>();
 
         //añadir valores al hashmap
-        numeros.put("uno", "1");
-        numeros.put("dos", "2");
-        numeros.put("tres", "3");
-        numeros.put("cuatro", "4");
-        //sout con hashmap
+        numeros.put("dos", 2);
+        numeros.put("tres", 3);
+        numeros.put("cuatro", 4);
+        numeros.put("cinco", 5);
+        numeros.put("seis", 6);
+        numeros.put("uno", 1);
         System.out.println(numeros);
+//Ordenar/crear el treee
+        SortedSet<Integer> valores = new TreeSet<>(numeros.values());
+        Iterator<Integer> iterator = valores.iterator();
+//print del tree
+        System.out.print("Tree set data: ");
+        while (iterator.hasNext())
+            System.out.print(iterator.next() + " ");
+        System.out.println();
 
-        //creamos treemap de strings asignar el hashmap
-        TreeMap<String, String> tm = new TreeMap<String, String>(numeros);
-        Iterator itr = tm.keySet().iterator();
-        while (itr.hasNext()) {
-
-            //preguntar key si es el string0? o que es?
-            String key = (String) itr.next();
-            System.out.println("key: " + key + " ,value: " + numeros.get(key));
-        }
-        ArrayList<HashMap<String, String>> arrayListHashMap = new ArrayList<HashMap<String, String>>();
-        arrayListHashMap.add(numeros);
-        Iterator<HashMap<String, String>> it = arrayListHashMap.iterator();
-
-        while (it.hasNext()) {
-            System.out.println(it.next());
+        // check si esta vacio
+        if (valores.isEmpty())
+            System.out.print("Tree Set is empty.");
+        else
+            System.out.println("Tree Set size: " + valores.size());
 
 
-        }
     }
-    }
+
+
+}
 
