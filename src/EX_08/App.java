6b642240189c927315c4d888a879d8485cceaddc
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-09-29 12:48:29 +0200

    EX_08

--- src/EX_08/App.java
+++ src/EX_08/App.java
@@ -1,7 +1,14 @@
 package EX_08;
-
+import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
-
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(4);
+        numeros.add(4);
+        numeros.forEach( (n) -> { System.out.println(n); } );
     }
 }
